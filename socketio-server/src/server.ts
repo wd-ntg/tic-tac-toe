925e227850a225dc683ec559175e4fbcdba57a1f
wd-ntg <giangnguyen6777@gmail.com>
2024-09-22 23:24:13 +0700

    Update config for server 4

--- socketio-server/src/server.ts
+++ socketio-server/src/server.ts
@@ -14,7 +14,7 @@ import socketServer from "./socket";
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || "9000");
+var port = normalizePort("9000");
 app.set("port", port);
 
 /**
